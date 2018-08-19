d4085c184dc621cc08a17fcc1a3e1d014ed623a9
Theodore Hu <theodore.n.hu@gmail.com>
2018-08-19 05:13:09 -0700

    Make background page function async

--- popup.js
+++ popup.js
@@ -10,17 +10,22 @@ window.onload=function() {
         elem.appendChild(node);
         document.body.appendChild(elem);
 
-        if (chrome.extension.getBackgroundPage().isTabOpen(name))
-          elem.style.color = "silver";
-        chrome.extension.getBackgroundPage().openTab(elem, url, name);
+        chrome.runtime.getBackgroundPage(function(bg) {
+          if (bg.isTabOpen(name))
+            elem.style.color = "silver";
+          bg.openTab(elem, url, name);
+        });
 
         var remElem = document.createElement("p");
         var remNode = document.createTextNode("-");
         remElem.style.color = "red";
         remElem.appendChild(remNode);
         document.body.appendChild(remElem);
-        chrome.extension.getBackgroundPage().removeBookmark(remElem, name, function() {
-            window.close();
+
+        chrome.runtime.getBackgroundPage(function(bg) {
+          bg.removeBookmark(remElem, name, function() {
+              window.close();
+          });
         });
       }
     });
@@ -32,14 +37,10 @@ window.onload=function() {
         chrome.tabs.query({'active': true, 'currentWindow': true}, function(tabs) {
           var newURL = tabs[0].url;
 
-          chrome.extension.getBackgroundPage().addBookmark(newName, newURL);
-
-          var newElem = document.createElement("p");
-          var newNode = document.createTextNode(newName);
-          newElem.appendChild(newNode);
-          document.body.insertBefore(newElem, document.body.firstChild.nextSibling.nextSibling.nextSibling.nextSibling);
-
-          chrome.extension.getBackgroundPage().open[tabs[0].id] = newName;
+          chrome.runtime.getBackgroundPage(function(bg) {
+            bg.addBookmark(newName, newURL);
+            bg.open[tabs[0].id] = newName;
+          });
           window.close();
         });
     });
