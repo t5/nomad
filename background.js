0f4cca3c0f0bac7f8dd876efc7935bc8fd32fe8b
Theodore Hu <theodore.n.hu@gmail.com>
2018-08-20 18:34:55 -0700

    Create UI

--- background.js
+++ background.js
@@ -1,61 +1,64 @@
 chrome.runtime.onInstalled.addListener(function() {
-    // this is a map from ID to name
-    open = {};
-    
-    bookmarks = {reddit: "https://reddit.com", 
-                google: "https://google.com", 
-                ucla: "https://ucla.edu"};
-    chrome.storage.sync.set({"bookmarks": bookmarks});
+  // this is a map from ID to name
+  open = {};
+  
+  bookmarks = {reddit: "https://reddit.com", 
+              google: "https://google.com", 
+              ucla: "https://ucla.edu"};
+  chrome.storage.sync.set({"bookmarks": bookmarks});
 });
+
+function updateTitleFav(tabId) {
+  chrome.tabs.executeScript(tabId, {file: 'contentScript.js'}, function() {
+    chrome.tabs.sendMessage(tabId, {tabName: open[tabId],
+                                    fav: chrome.runtime.getURL("icons/favicon.ico")});
+  });
+}
 
 // when bookmark tabs are updated, open obj is also updated
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (open.hasOwnProperty(tabId) && changeInfo.status == "complete") {
       // change the favicon and title
-      chrome.tabs.executeScript(tabId, {file: 'contentScript.js'}, function() {
-        chrome.tabs.sendMessage(tabId, {tabName: open[tabId],
-                                        fav: chrome.runtime.getURL("icons/favicon.ico")});
-      });
+      updateTitleFav(tabId);
   }
   if (changeInfo.url && open.hasOwnProperty(tabId)) {
-      chrome.storage.sync.get(["bookmarks"], function(result) {
-        var currBookmarks = result.bookmarks;
-        var currName = open[tabId];
-        currBookmarks[currName] = changeInfo.url;
-        chrome.storage.sync.set({"bookmarks": currBookmarks});
-      });
+    chrome.storage.sync.get(["bookmarks"], function(result) {
+      var currBookmarks = result.bookmarks;
+      var currName = open[tabId];
+      currBookmarks[currName] = changeInfo.url;
+      chrome.storage.sync.set({"bookmarks": currBookmarks});
+    });
   }
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
   // access the array of open tabs
   if (open.hasOwnProperty(tabId)) {
-      // remove the closed tab from the open tabs
-      delete open[tabId];
+    // remove the closed tab from the open tabs
+    delete open[tabId];
   }
 });
 
 function openTab(elem, url, name){
-    elem.addEventListener("click", function() {
-      
-      // check if tab open
-      if (isTabOpen(name)) {
-        // make tab active
-        var clickedTabId = Object.entries(open).find(i => i[1] === name);
-        chrome.tabs.get(parseInt(clickedTabId[0], 10), function(tab) {
-          chrome.tabs.update(tab.id, {highlighted: true});
-          return;
-        });
-      }
-      else {
-        // if the link button is clicked
-        var newTab = url;
-        chrome.tabs.create({url: newTab}, function(tab) { 
-          // after creating the tab, add it's ID and URL to open
-            open[tab.id] = name;
-        });
-      }
-    });
+  elem.addEventListener("click", function() {
+    // check if tab open
+    if (isTabOpen(name)) {
+      // make tab active
+      var clickedTabId = Object.entries(open).find(i => i[1] === name);
+      chrome.tabs.get(parseInt(clickedTabId[0], 10), function(tab) {
+        chrome.tabs.update(tab.id, {highlighted: true});
+        return;
+      });
+    }
+    else {
+      // if the link button is clicked
+      var newTab = url;
+      chrome.tabs.create({url: newTab}, function(tab) { 
+        // after creating the tab, add it's ID and URL to open
+          open[tab.id] = name;
+      });
+    }
+  });
 }
 
 function addBookmark(name, url) {
@@ -67,14 +70,18 @@ function addBookmark(name, url) {
 }
 
 function removeBookmark(elem, name, callback) {
-    elem.addEventListener("click", function() {
-      chrome.storage.sync.get(["bookmarks"], function(result) {
-        currBookmarks = result.bookmarks;
-        delete currBookmarks[name];
-        chrome.storage.sync.set({"bookmarks": currBookmarks});
-        callback();
-      });
+  elem.addEventListener("click", function(event) {
+
+    // stop deletion from opening the tab
+    event.stopPropagation();
+
+    chrome.storage.sync.get(["bookmarks"], function(result) {
+      currBookmarks = result.bookmarks;
+      delete currBookmarks[name];
+      chrome.storage.sync.set({"bookmarks": currBookmarks});
+      callback();
     });
+  });
 }
 
 function isTabOpen(name) {
--- popup.js
+++ popup.js
@@ -1,46 +1,96 @@
-window.onload=function() {
 
+function setup() {
+    setupLinks();
+    setupBookmarkAdder();
+}
+
+function setupLinks() {
     // after loading the popup, add the links to bookmarks
     chrome.storage.sync.get(['bookmarks'], function(result) {
       for (var name in result.bookmarks) {
-        // add the url
-        var url = result.bookmarks[name];
-        var elem = document.createElement("p");
-        var node = document.createTextNode(name);
-        elem.appendChild(node);
-        document.body.appendChild(elem);
 
-        if (chrome.extension.getBackgroundPage().isTabOpen(name))
-          elem.style.color = "silver";
+        var listgroup = document.getElementsByClassName("list-group")[0];
+
+        // elem will contain nameDiv and buttonDiv, which will then be put in the list
+        var elem = document.createElement("li");
+        elem.classList.add("list-group-item");
+        elem.classList.add("list-group-item-action");
+
+        var nameDiv = document.createElement("div");
+        var node = document.createTextNode(name);
+
+        nameDiv.appendChild(node);
+        if (chrome.extension.getBackgroundPage().isTabOpen(name)) {
+          nameDiv.appendChild(document.createTextNode(" (open)"));
+        }
+        elem.appendChild(nameDiv);
+
+        // make sure name doesn't overflow
+        nameDiv.style.cssText = "text-overflow: ellipsis; overflow: hidden; white-space: nowrap;";
+        nameDiv.classList.add("pr-2");
+
+        // add the url and make it clickable
+        var url = result.bookmarks[name];
         chrome.extension.getBackgroundPage().openTab(elem, url, name);
 
-        var remElem = document.createElement("p");
-        var remNode = document.createTextNode("-");
-        remElem.style.color = "red";
-        remElem.appendChild(remNode);
-        document.body.appendChild(remElem);
+        var buttonDiv = document.createElement("div");
+        var remElem = document.createElement("button");
+        var remText = document.createTextNode("Delete");
+        remElem.appendChild(remText);
+        remElem.setAttribute("type", "button");
+        remElem.classList.add("btn");
+        remElem.classList.add("btn-outline-danger");
+        remElem.classList.add("text-right");
+        remElem.classList.add("btn-sm");
+        buttonDiv.appendChild(remElem);
+        elem.appendChild(buttonDiv);
+
+        // make the bookmarks removable
         chrome.extension.getBackgroundPage().removeBookmark(remElem, name, function() {
-            window.close();
+          location.reload();           
         });
+
+        // make it flex
+        elem.classList.add("d-flex"); 
+        elem.classList.add("justify-content-between"); 
+        elem.classList.add("align-items-center"); 
+        elem.style.cssText = "min-height: 50px"; 
+
+        listgroup.appendChild(elem);
       }
     });
+}
 
-    // add new bookmark
-    isNewBookmark = document.getElementById("add_bookmark");
-    isNewBookmark.addEventListener("click", function() {
-        var newName = document.getElementById("bname").value;
-        chrome.tabs.query({'active': true, 'currentWindow': true}, function(tabs) {
-          var newURL = tabs[0].url;
 
-          chrome.extension.getBackgroundPage().addBookmark(newName, newURL);
+function setupBookmarkAdder() {
+  // when Enter is pressed, add a bookmark
+  var textField = document.getElementById("bname");
+  textField.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      document.getElementById("add_bookmark").click();
+    }
+  });
 
-          var newElem = document.createElement("p");
-          var newNode = document.createTextNode(newName);
-          newElem.appendChild(newNode);
-          document.body.insertBefore(newElem, document.body.firstChild.nextSibling.nextSibling.nextSibling.nextSibling);
+  // add new bookmark
+  isNewBookmark = document.getElementById("add_bookmark");
+  isNewBookmark.addEventListener("click", function() {
+      var newName = document.getElementById("bname").value;
 
-          chrome.extension.getBackgroundPage().open[tabs[0].id] = newName;
-          window.close();
-        });
-    });
+      chrome.tabs.query({'active': true, 'currentWindow': true}, function(tabs) {
+        var newURL = tabs[0].url;
+
+        if (newName === "") {
+            newName = tabs[0].title;
+        }
+
+        chrome.extension.getBackgroundPage().addBookmark(newName, newURL);
+        chrome.extension.getBackgroundPage().open[tabs[0].id] = newName;
+        chrome.extension.getBackgroundPage().updateTitleFav(tabs[0].id);
+        location.reload();
+      });
+  });
+}
+
+window.onload=function() {
+    setup();
 }
