313d6d63d4bca15cf5ac7de779fae29bc877517e
Theodore Hu <theodore.n.hu@gmail.com>
2018-08-21 03:00:21 -0700

    Add usability improvements

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  document.title = "\u2A65 " + request.tabName + " \u2A64";
+  document.title = request.tabName;
 
   // remove all favicons
   document.querySelectorAll("link[rel*='icon']").forEach(x => x.parentNode.removeChild(x));
--- popup.js
+++ popup.js
@@ -18,12 +18,19 @@ function setupLinks() {
 
         var nameDiv = document.createElement("div");
         var node = document.createTextNode(name);
-
         nameDiv.appendChild(node);
-        if (chrome.extension.getBackgroundPage().isTabOpen(name)) {
-          nameDiv.appendChild(document.createTextNode(" (open)"));
-        }
         elem.appendChild(nameDiv);
+
+        if (chrome.extension.getBackgroundPage().isTabOpen(name)) {
+          var openDiv = document.createElement('div');
+          openDiv.classList.add("badge");
+          openDiv.classList.add("badge-primary");
+          openDiv.classList.add("badge-pill");
+          openDiv.textContent = "open";
+          openDiv.classList.add("mr-auto");
+          openDiv.classList.add("mr-1");
+          elem.appendChild(openDiv);
+        }
 
         // make sure name doesn't overflow
         nameDiv.style.cssText = "text-overflow: ellipsis; overflow: hidden; white-space: nowrap;";
@@ -42,6 +49,7 @@ function setupLinks() {
         remElem.classList.add("btn-outline-danger");
         remElem.classList.add("text-right");
         remElem.classList.add("btn-sm");
+        buttonDiv.classList.add("ml-2");
         buttonDiv.appendChild(remElem);
         elem.appendChild(buttonDiv);
 
@@ -53,6 +61,7 @@ function setupLinks() {
         // make it flex
         elem.classList.add("d-flex"); 
         elem.classList.add("justify-content-between"); 
+        // make text vertically center
         elem.classList.add("align-items-center"); 
         elem.style.cssText = "min-height: 50px"; 
 
@@ -65,6 +74,11 @@ function setupLinks() {
 function setupBookmarkAdder() {
   // when Enter is pressed, add a bookmark
   var textField = document.getElementById("bname");
+
+  // make it automatically focused
+  textField.focus();
+  textField.style.cssText = "outline: 0 none; box-shadow: none;";
+
   textField.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
       document.getElementById("add_bookmark").click();
@@ -75,18 +89,53 @@ function setupBookmarkAdder() {
   isNewBookmark = document.getElementById("add_bookmark");
   isNewBookmark.addEventListener("click", function() {
       var newName = document.getElementById("bname").value;
+      
+      chrome.storage.sync.get(["bookmarks"], function(result) {
+        var currBookmarks = result.bookmarks;
+        if (currBookmarks[newName]) {
+          // first remove all previous alerts
+          document.querySelectorAll("div[class*='alert']").forEach(x => x.parentNode.removeChild(x));
 
-      chrome.tabs.query({'active': true, 'currentWindow': true}, function(tabs) {
-        var newURL = tabs[0].url;
+          var newAlert = document.createElement("div");
+          newAlert.setAttribute("role", "alert");
+          newAlert.textContent = "\"" + newName + "\" has already been taken."
+          newAlert.classList.add("alert");
+          newAlert.classList.add("alert-danger");
+          newAlert.classList.add("alert-dismissible");
+          newAlert.classList.add("alert-fade");
+          newAlert.classList.add("alert-show");
 
-        if (newName === "") {
-            newName = tabs[0].title;
+          // add dismiss button
+          var dismissSpan = document.createElement("span");
+          dismissSpan.setAttribute("aria-hidden", "true");
+          dismissSpan.textContent = "×";
+
+          var dismissButton = document.createElement("button");
+          dismissButton.setAttribute("data-dismiss", "alert");
+          dismissButton.setAttribute("aria-label", "Close");
+          dismissButton.setAttribute("type", "button");
+          dismissButton.classList.add("close");
+          dismissButton.appendChild(dismissSpan);
+
+          newAlert.appendChild(dismissButton);
+
+          document.getElementsByClassName("card-header")[0].appendChild(newAlert);
+          return;
         }
+        else {
+          chrome.tabs.query({'active': true, 'currentWindow': true}, function(tabs) {
+            var newURL = tabs[0].url;
 
-        chrome.extension.getBackgroundPage().addBookmark(newName, newURL);
-        chrome.extension.getBackgroundPage().open[tabs[0].id] = newName;
-        chrome.extension.getBackgroundPage().updateTitleFav(tabs[0].id);
-        location.reload();
+            if (newName === "") {
+                newName = tabs[0].title;
+            }
+
+            chrome.extension.getBackgroundPage().addBookmark(newName, newURL);
+            chrome.extension.getBackgroundPage().open[tabs[0].id] = newName;
+            chrome.extension.getBackgroundPage().updateTitleFav(tabs[0].id);
+            location.reload();
+          });
+        }
       });
   });
 }
